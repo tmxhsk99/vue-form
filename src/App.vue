c9ff88c7454d324b5efaa09101d7ea353c200353
tmxhsk99 <tmxhsk99@naver.com>
2022-07-13 05:24:23 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
-<form>
-  <div>
-    <label for="username">id:</label>
-    <input id="username" type="text">
-  </div>
-  <div>
-    <label for="password">pw: </label>
-    <input id="password" type="password">
-  </div>
-  <button>login</button>
-</form>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data: function () {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function(){
+      //event.preventDefault();
+      console.log(this.username,this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username : this.username,
+        password : this.password
+      }
+
+      axios.post(url,data)
+          .then(function(response){
+            console.log(response);
+          })
+          .catch(function(error){
+            console.log(error);
+          });
+    }
+  }
 }
 </script>
 
